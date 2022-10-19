77add2a83b28f77c13d2ac81b52706480cf41d11
许圣峰 <kricsleo@163.com>
2022-10-19 18:54:09 +0800

    feat: support chinese holiday

---
+++ src/composables/chinese-holidays.ts
@@ -0,0 +1,32 @@
+import { isWithinInterval } from 'date-fns'
+
+interface Holiday {
+  name: string
+  range: [string, string?]
+  type: 'workingday' | 'holiday'
+}
+
+const holidayJson: Record<string, Holiday[]> = import.meta.glob([
+  '/chinese-holidays-data/data/*.json',
+  '!**/index.json',
+], { eager: true, import: 'default' })
+
+export const holidays = Object.values(holidayJson).flat()
+
+export function isChineseHoliday(day: Date): boolean {
+  const holiday = findChineseDay(day)
+  return holiday?.type === 'holiday'
+}
+
+export function isChineseWorkingDay(day: Date): boolean {
+  const holiday = findChineseDay(day)
+  return holiday?.type === 'workingday'
+}
+
+export function findChineseDay(day: Date): Holiday | undefined {
+  return holidays.find(holiday => isWithinInterval(day, {
+    // `T00:00` converts date to local timezone
+    start: new Date(holiday.range[0] + 'T00:00'),
+    end: new Date((holiday.range[1] || holiday.range[0]) + 'T00:00'),
+  }))
+}
